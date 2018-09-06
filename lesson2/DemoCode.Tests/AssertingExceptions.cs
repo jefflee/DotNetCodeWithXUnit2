447463c12039a70bf1b56da2033e06c304a8fe7d
Jeff Lee <hellow.jeff@gmail.com>
2018-09-06 22:15:22 +0800

    categorise tests

--- lesson2/DemoCode.Tests/AssertingExceptions.cs
+++ lesson2/DemoCode.Tests/AssertingExceptions.cs
@@ -1,11 +1,14 @@
 ﻿using System;
+using System.Threading;
 using Xunit;
 
 namespace DemoCode.Tests
 {
+    [Trait("Category", "Error Checking")]
     public class AssertingExceptions
     {
         [Fact]
+
         public void ShouldErrorWhenDivideByZero()
         {
             var sut = new Calculator();
@@ -24,6 +27,21 @@ namespace DemoCode.Tests
 
             Assert.Equal("value", thrownException.ParamName);
 
+        }
+
+        [Fact]
+        [Trait("Category", "Slow Running")]
+        public void ShouldErrorWhenNumberTooBig_SLOW()
+        {
+            var sut = new Calculator();
+
+            // Simulate long running test
+            Thread.Sleep(TimeSpan.FromSeconds(2));
+
+            ArgumentOutOfRangeException thrownException =
+                Assert.Throws<ArgumentOutOfRangeException>(() => sut.Divide2(201, 1));
+
+            Assert.Equal("value", thrownException.ParamName);
         }
 
 
