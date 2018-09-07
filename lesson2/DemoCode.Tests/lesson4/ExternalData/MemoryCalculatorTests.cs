44736295aa99d4b87c5f17693320d5c8aefd924f
Jeff Lee <hellow.jeff@gmail.com>
2018-09-08 00:18:10 +0800

    Test data from external source.

---
+++ lesson2/DemoCode.Tests/lesson4/ExternalData/MemoryCalculatorTests.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Xunit;
+
+namespace DemoCode.Tests.ExternalData
+{
+    public class MemoryCalculatorTestData
+    {
+        public static IEnumerable<object[]> TestData
+        {
+            get
+            {
+                string[] csvLines = File.ReadAllLines("lesson4\\ExternalData\\TestData.csv");
+
+                var testCases = new List<object[]>();
+
+                foreach (var csvLine in csvLines)
+                {
+                    IEnumerable<int> values = csvLine.Split(',').Select(int.Parse);
+
+                    object[] testCase = values.Cast<object>().ToArray();
+
+                    testCases.Add(testCase);
+                }
+
+                return testCases;
+            }
+        }
+    }
+
+    public class MemoryCalculatorTests
+    {
+        [Theory]
+        [MemberData("TestData", MemberType = typeof(MemoryCalculatorTestData))]
+        public void ShouldSubtractTwoNumbers(int firstNumber, int secondNumber, int expectedResult)
+        {
+            var sut = new MemoryCalculator();
+
+            sut.Subtract(firstNumber);
+            sut.Subtract(secondNumber);
+
+            Assert.Equal(expectedResult, sut.CurrentValue);
+        }
+    }
+}
