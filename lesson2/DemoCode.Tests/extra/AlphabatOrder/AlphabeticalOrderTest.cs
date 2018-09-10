02024179e6100d4aed05b431db319f9257f91a6b
Jeff Lee <hellow.jeff@gmail.com>
2018-09-11 00:10:04 +0800

    add more tests

---
+++ lesson2/DemoCode.Tests/extra/AlphabatOrder/AlphabeticalOrderTest.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace DemoCode.Tests.extra.AlphabatOrder
+{
+    [TestCaseOrderer("DemoCode.Tests.extra.AlphabatOrder.AlphabeticalOrderer", "DemoCode.Tests")]
+    public class AlphabeticalOrderTest
+    {
+        public static bool Test1Called;
+        public static bool Test2Called;
+        public static bool Test3Called;
+
+        [Fact]
+        public void Test1()
+        {
+            Test1Called = true;
+
+            Assert.False(Test2Called);
+            Assert.False(Test3Called);
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            Test2Called = true;
+
+            Assert.True(Test1Called);
+            Assert.False(Test3Called);
+        }
+
+        [Fact]
+        public void Test3()
+        {
+            Test3Called = true;
+
+            Assert.True(Test1Called);
+            Assert.True(Test2Called);
+        }
+    }
+}
---
+++ lesson2/DemoCode.Tests/lesson4/DatasourceAttribute/DatasourceAttributeTests.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using Xunit.Sdk;
+
+namespace DemoCode.Tests.lesson4.DatasourceAttribute
+{
+    public class CsvTestDataAttribute : DataAttribute
+    {
+        private readonly string _csvFileName;
+
+        public CsvTestDataAttribute(string csvFileName)
+        {
+            _csvFileName = csvFileName;
+        }
+
+        public override IEnumerable<object[]> GetData(MethodInfo testMethod)
+        {
+            string[] csvLines = File.ReadAllLines(_csvFileName);
+
+            var testCases = new List<object[]>();
+
+            foreach (var csvLine in csvLines)
+            {
+                IEnumerable<int> values = csvLine.Split(',').Select(int.Parse);
+
+                object[] testCase = values.Cast<object>().ToArray();
+
+                testCases.Add(testCase);
+            }
+
+            return testCases;
+        }
+    }
+
+    public class MemoryCalculatorTests
+    {
+        [Theory]
+        [CsvTestData("lesson4\\ExternalData\\TestData.csv")]
+        public void ShouldSubtractTwoNumbers(int firstNumber, int secondNumber, int expectedResult)
+        {
+            var sut = new MemoryCalculator();
+
+            sut.Subtract(firstNumber);
+            sut.Subtract(secondNumber);
+
+            Assert.Equal(expectedResult, sut.CurrentValue);
+        }
+    }
+}
